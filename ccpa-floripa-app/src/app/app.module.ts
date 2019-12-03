b45b3c70529f9d605fb9053e4c71b55c50074e64
Guilherme Wesley dos Santos da Costa <guilherme.costa@tivit.com>
2019-12-03 10:43:53 -0300

    Configurando projeto Angular + Firebase

---
+++ ccpa-floripa-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ccpa-floripa-app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAbC8YYYw1QaCH5t7JcwUUDyjt9hsB5Nqo",
+    authDomain: "ccpa-floripa.firebaseapp.com",
+    databaseURL: "https://ccpa-floripa.firebaseio.com",
+    projectId: "ccpa-floripa",
+    storageBucket: "ccpa-floripa.appspot.com",
+    messagingSenderId: "994573777688",
+    appId: "1:994573777688:web:a44142b6e109220f69fc6d",
+    measurementId: "G-66VKL2Q54R"
+  }
+};
+
