e659945801b932635324ce579286ed00a2b258b8
Guilherme Wesley dos Santos da Costa <guilherme.costa@tivit.com>
2019-12-27 16:04:27 -0300

    Adicionado módulo de login pra testes.

--- ccpa-floripa-app/src/app/app.module.ts
+++ ccpa-floripa-app/src/app/app.module.ts
@@ -5,8 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FirebaseUIModule } from 'firebaseui-angular';
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+ 
 import { AngularFireModule } from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+
 import { environment } from '../environments/environment';
 import { EditComponent } from './produto/edit/edit.component';
 import { ListComponent } from './produto/list/list.component';
@@ -21,6 +27,36 @@ import { CalendarioComponent } from './calendarios/calendario/calendario.compone
 
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { EventoComponent } from './calendarios/evento/evento.component';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+};
 
 @NgModule({
   declarations: [
@@ -43,6 +79,8 @@ import { EventoComponent } from './calendarios/evento/evento.component';
     AngularFireDatabaseModule,
     BrowserAnimationsModule,
     FullCalendarModule,
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
