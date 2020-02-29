7cf0665e33cbbc9699e1769d94fa3d472b84202d
Manolinho009 <thiagohenrique166@gmail.com>
2020-02-29 13:41:47 -0300

    Mudanças no carrossel da home

--- ccpa-floripa-app/src/app/home/carrossel/carrossel.component.ts
+++ ccpa-floripa-app/src/app/home/carrossel/carrossel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ImagemService } from 'src/app/galerias/imagem.service';
+import { Imagem } from 'src/app/galerias/imagem';
 
 @Component({
   selector: 'app-carrossel',
