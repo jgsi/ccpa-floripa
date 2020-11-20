69d883b7eb347d6a96ece262cd62e50983f91a0e
manolinho009 <thiagohenrique16@gmail.com>
2020-11-20 19:57:39 -0300

    Add a parte q esconde o botão do add escala

--- ccpa-floripa-app/src/app/escalas/escalas.component.ts
+++ ccpa-floripa-app/src/app/escalas/escalas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAction } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DepartamentoService } from '../departamentos/departamento.service';
 import { Escala } from './escala';
 import { EscalaDataService } from './escala-data.service';
@@ -17,13 +18,20 @@ export class EscalasComponent implements OnInit {
   escala : Escala
   key : string = ''
 
-  constructor(private departamentoService : DepartamentoService, private escalaService: EscalaService,private escalaDataService: EscalaDataService) {}
+  constructor(private authService : AuthService,private departamentoService : DepartamentoService, private escalaService: EscalaService,private escalaDataService: EscalaDataService) {}
 
   ngOnInit() {
     this.escala = new Escala()
     this.departamentos$ = this.departamentoService.getAll();
   }
 
+  usuarioLogado(){
+    try{
+      return this.authService.isLoggedIn ? '    '+ this.authService.userData.displayName : ''
+    } catch (error){
+     // console.error(error)
+    }
+  }
 
 
   new(){
